03c53866b5aabb2ac37352bf9668518aebe2c9ae
Shahrukh <mdsrk8499@gmail.com>
2021-04-26 16:06:54 +0530

    hello This is program 2

---
+++ eclipse-workspace/Electricity/src/InvalidConsumerNumberException.java
@@ -0,0 +1,6 @@
+@SuppressWarnings("serial")
+public class InvalidConsumerNumberException extends Exception {
+    public InvalidConsumerNumberException(String message) {
+        super(message);
+    }
+}
